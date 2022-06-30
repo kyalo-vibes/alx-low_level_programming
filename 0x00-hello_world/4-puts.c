0bdd25a19c05e0be757122cc2bba92d65719693c
kyalo-vibes <kevin.kioko4@gmail.com>
2022-06-30 12:34:19 -0700

    Change 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 /**
  
-    Description: main - Prints out a string to stdout.
+   * Description: main - Prints out a string to stdout.
  
-    Return: 0 if success.
+  *  Return: 0 if success.
  
  
      */
