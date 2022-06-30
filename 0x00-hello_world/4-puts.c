4db22318041ef982a1f95ef3d9d979640029ba7d
kyalo-vibes <kevin.kioko4@gmail.com>
2022-06-30 12:28:15 -0700

    Necessary modifaction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-int main(void) /* begins proramm */
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
 
-{	/**
+int main(void)
 
-	  	* puts function prints something to the screen.
-		*
-		* 	**/
+{
 
-		puts("\"programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 
-				return (0);
+			return (0);
 
 }
