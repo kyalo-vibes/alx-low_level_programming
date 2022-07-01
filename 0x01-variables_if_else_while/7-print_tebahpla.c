c4d36087ec3290ef5dab872d29394390652df89f
kyalo-vibes <95200602+kyalo-vibes@users.noreply.github.com>
2022-07-01 20:49:40 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**                                                                             
- *  * main - program that prints the lowercase alphabet in reverse.                
- *   * You can only use the putchar function                                        
- *    * Return: 0                                                                    
- *     */
+* main - program that prints the lowercase alphabet in reverse.                
+* You can only use the putchar function                                        
+* Return: 0                                                                    
+*/
 
 int main(void)
 {
