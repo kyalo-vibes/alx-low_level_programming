7ee9df18b7de0e0068ee6bd5e84aecafe54c663b
kyalo-vibes <kevin.kioko4@gmail.com>
2022-07-05 10:59:36 -0700

    Second try

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,10 @@
 #include "main.h"
 
 /**
- *
- * *  * print_sign - function that prints the sign of a number..
- *
- * *   * @n: The input number to check
- *
- * *    * Return: int.
- *
- * */
+* print_sign - function that prints the sign of a number..
+* @n: The input number to check
+* Return: int.
+*/
 
 int print_sign(int n)
 {
