a8e430fa2fa051d2615fc293336247bf006b7d85
kyalo-vibes <kevin.kioko4@gmail.com>
2022-07-05 09:07:07 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the whole lowercase alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int l = 'a';
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l += 1;
+	}
+	_putchar(10);
+}
+
