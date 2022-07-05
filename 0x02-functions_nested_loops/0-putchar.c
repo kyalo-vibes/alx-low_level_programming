e5fcc0d550817fde655692e048fff9e967ce70b3
kyalo-vibes <kevin.kioko4@gmail.com>
2022-07-05 00:37:57 -0700

    First try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/**
+ * main - program that prints _putchar
+ * Return: int
+ * /
+int main(void)
+{
+int k = 0;
+char str_sol[8] = "_putchar";
+while(k < 9)
+{
+_putchar(str_sol[n]);
+k++;
+}
+_putchar('\n');
+return (0);
+}
+
