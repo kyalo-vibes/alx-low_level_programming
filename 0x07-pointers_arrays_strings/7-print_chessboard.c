f1afd97208a47ac22a9800968a9c1879c08d88a0
kyalo-vibes <kevin.kioko4@gmail.com>
2022-07-18 04:50:34 -0700

    Trial one

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,21 +9,17 @@
 void print_chessboard(char (*a)[8])
 {
 	int i = 0;
-	int j;
 
 	while (i < 8)
 	{
-		j = 0;
+		int j = 0;
 
 		while (j < 8)
 		{
 			_putchar(a[i][j]);
-			if (j == 7)
-			{
-				_putchar('\n');
-			}
 			j++;
 		}
+		_putchar('\n');
 		i++;
 	}
 }
