2899e67cda0e60f564f6ca6ce375103c17f23829
kyalo-vibes <kevin.kioko4@gmail.com>
2022-07-22 01:06:00 -0700

    Mandatory tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - function
+ *@argc: parameter 1
+ *@argv: parameter2
+ *Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	while (i < argc)
+	{
+		i++;
+	}
+	printf("%d\n", --i);
+	return (0);
+}
